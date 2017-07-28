72cfdae15581ae9dba8e8ef15e14e818a15d5e05
jfox <fox.jonny@gmail.com>
2017-07-28 16:56:48 +0200

    publish done

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+// Run the app by serving the static files
+// in the dist directory
+app.use(express.static(__dirname + '/dist'));
+// Start the app by listening on the default
+// Heroku port
+
+// For all GET requests, send back index.html
+// so that PathLocationStrategy can be used
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname + '/dist/index.html'));
+});
+app.listen(process.env.PORT || 8080);
